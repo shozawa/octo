00afb441d790af073ff07449eea52226a7284d34
waki_k <kyosuke.w@gmail.com>
2016-02-14 15:34:58 +0900

    rootをwelcomeに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   get 'users/new'
   get 'signup'  => 'users#new'
   resources :users
@@ -6,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#new'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
