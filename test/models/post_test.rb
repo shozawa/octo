c9614977095672ac1a7d2f1af80961c3a400f76a
shozawa tomohiro <t.shozawa@gmail.com>
2016-02-20 16:58:33 +0900

    ajaxでコメントを投稿できるようにしました

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post = Post.new(content: "test", version_id: 1)
+  end
+  test "正しくpostが作成される" do
+    @post.valid?
+  end
 end
