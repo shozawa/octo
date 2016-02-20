e8a8a2e181f45612e4a1be514d22d376a5f017e8
waki_k <kyosuke.w@gmail.com>
2016-02-20 20:55:31 +0900

    ログイン前のページへリダイレクト

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,10 +7,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @other_user = users(:archer)
   end
 
-  test "unsuccessful edit" do
-    log_in_as(@user)
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
     name  = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), user: { name:  name,
