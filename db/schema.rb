96ff1a84da20a0555c90e971b32771c489a7ccec
waki_k <kyosuke.w@gmail.com>
2016-02-26 23:42:37 +0900

    userモデルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221075810) do
+ActiveRecord::Schema.define(version: 20160226143954) do
 
   create_table "boards", force: :cascade do |t|
     t.datetime "created_at",     null: false
@@ -44,21 +44,6 @@ ActiveRecord::Schema.define(version: 20160221075810) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
-    t.string   "password_digest"
-    t.string   "remember_digest"
-    t.boolean  "admin",             default: false
-    t.string   "activation_digest"
-    t.boolean  "activated",         default: false
-    t.datetime "activated_at"
-    t.string   "reset_digest"
-    t.datetime "reset_sent_at"
   end
 
   create_table "versions", force: :cascade do |t|
