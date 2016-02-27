a0bdf75a9749a44ed11923f540c4e0146ed9be75
waki_k <kyosuke.w@gmail.com>
2016-02-27 23:53:03 +0900

    s3設定

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'rails_admin'
 gem 'html2slim'
 gem 'carrierwave'
 gem 'fog'
+gem 'dotenv-rails'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,24 +1,34 @@
+#Fog.credentials_path = Rails.root.join('config/s3.yml')
+#s3_conf = YAML.load_file("#{Rails.root}/config/s3.yml")[Rails.env]
+#Fog.credentials_path = YAML.load(File.read("#{Rails.root}/config/s3.yml"))[Rails.env]
+
+#日本語ファイル
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+
 CarrierWave.configure do |config|
+  # config_file = File.join(Rails.root, 'config', 's3.yml')
+  # APP_CONFIG = YAML.load(File.open(config_file))[Rails.env]
+  # access_key = APP_CONFIG[:aws_access_key_id]
+  # secret_key = APP_CONFIG[:aws_secret_access_key]
+  # region = APP_CONFIG[:region]
+
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'AKIAIXI2XT3XPRIQY3JQ',
-    aws_secret_access_key: '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',
-    region: 'ap-northeast-1'
+    aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+    aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+    region: ENV['AWS_REGION']
   }
   config.fog_public = false
   config.fog_authenticated_url_expiration = 60
 
   case Rails.env
     when 'production'
-      config.fog_directory = 'octfilestorage'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/octfilestorage'
+      config.fog_directory = "octfilestorage"
 
     when 'development'
-      config.fog_directory = 'dev.octfilestorage'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/dev.octfilestorage'
+      config.fog_directory = "dev.octfilestorage"
 
     when 'test'
-      config.fog_directory = 'test.octfilestorage'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/test.octfilestorage'
+      config.fog_directory = "test.octfilestorage"
   end
 end
