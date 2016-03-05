163abcef24c60c5975770da4a1485929f6b0ef06
waki_k <kyosuke.w@gmail.com>
2016-03-06 00:45:44 +0900

    メッセージ日本語化など

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :set_locale
   before_action :authenticate_user!
   protect_from_forgery with: :exception
   http_basic_authenticate_with :name => 'octo', :password => 'admin0910' if Rails.env == "production"
+  
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
 end
